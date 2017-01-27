c3235b7febeb5e291499ea5eb56af75aab0064b7
aedan333 <gageaedan333@gmail.com>
2017-01-26 19:57:23 -0700

    Major fixes

--- scripts10.js
+++ scripts10.js
@@ -1,5 +1,54 @@
 $(function(){   
     
+    $('.js--show-alert').click(function(){
+		$('.js--alert').slideDown("fast");
+	});
+
+	$('.js--alert-close').click(function(){
+		$('.js--alert').slideUp("fast");
+	});
+
+	
+
+	$('.js--show-alert2').click(function(){
+		$('.js--alert2').slideDown("fast");
+	});
+
+	$('.js--alert-close2').click(function(){
+		$('.js--alert2').slideUp("fast");
+	});
+
+	
+
+	$('.js--show-alert3').click(function(){
+		$('.js--alert3').slideDown("fast");
+	});
+
+	$('.js--alert-close3').click(function(){
+		$('.js--alert3').slideUp("fast");
+	});
+
+	
+
+	$('.js--show-alert4').click(function(){
+		$('.js--alert').slideDown("fast");
+	});
+
+	$('.js--alert-close').click(function(){
+		$('.js--alert').slideUp("fast");
+	});
+
+	
+
+	$('.js--show-alert5').click(function(){
+		$('.js--alert').slideDown("fast");
+	});
+
+	$('.js--alert-close').click(function(){
+		$('.js--alert').slideUp("fast");
+	});
+
+
     $(".button").click(function(){
 	alert("this worked!");
      });
